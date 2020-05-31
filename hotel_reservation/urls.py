070807c5ac9d9b2abca91f20baef2e48e964005b
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-31 13:07:26 +0430

    use account urls in main urls.py to forward requests

--- hotel_reservation/urls.py
+++ hotel_reservation/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('blog.urls', 'blog'), namespace="blog")),
+    path('accounts/',include('AccountApp.urls'),name='login'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
