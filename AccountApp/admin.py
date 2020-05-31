6f680c36250fefb3fb3692acbb1f7afc90f6b1a5
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-31 13:21:56 +0430

    add profile to admin part

--- AccountApp/admin.py
+++ AccountApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','firstname','lastname','phone','email','national_id']
+
+admin.site.register(Profile,ProfileAdmin)
