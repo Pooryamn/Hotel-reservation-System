a6f4d3a7306ec2d9572efa640c33d21dd084d746
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-31 17:37:38 +0430

    remove some fields from profile model

--- AccountApp/models.py
+++ AccountApp/models.py
@@ -4,11 +4,8 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    firstname = models.CharField(max_length=50,blank=False)
-    lastname = models.CharField(max_length=70,blank=False)
     phone = models.CharField(max_length=15,blank=False)
-    email = models.CharField(max_length=100,blank=False)
     national_id = models.CharField(max_length=10,blank=False)
 
     def __str__(self):
-        return self.firstname + ' ' + self.lastname
+        return self.national_id
