9ec99075514472ced56e398d8bda70cca4f6484b
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-06-03 00:03:50 +0430

    add url for controlling confrim codes

--- AccountApp/urls.py
+++ AccountApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # post views
     url('login/',views.user_login,name='login'),
     url('register/',views.register,name='register'),
+    url('Confrim_check/',views.Confrim_check,name='Confrim_check'),
 ]
