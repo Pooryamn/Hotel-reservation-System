369165baec784ef109141bcb1065a2fe95c0c1ba
mahdigh8 <mahdighazaei8@gmail.com>
2020-06-03 01:02:14 +0430

    change login and register page design

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'blog',
     'jalali_date',
     'AccountApp',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -156,6 +157,11 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
