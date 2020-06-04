8008b692b1b910298306d80add5fb4896bc62544
mahdigh8 <mahdighazaei8@gmail.com>
2020-06-04 23:55:29 +0430

    update on blog ulrs

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 # app_name = "blog"
 
 urlpatterns = [
-    path('hotel/<int:id>', views.hotel_detail, name='hotel_detail'),
-    path('hotel/<int:id>/reserve', views.hotel_reserve, name='hotel_reserve'),
-    path('hotel/search', views.search, name="search"),
-    path('hotel/reserve-factor/<str:tracking_code>', views.reserve_factor, name='reserve_factor'),
+    path('<int:id>/', views.hotel_detail, name='hotel_detail'),
+    path('<int:id>/reserve/', views.hotel_reserve, name='hotel_reserve'),
+    path('search/', views.search, name="search"),
+    path('reserve-factor/<str:tracking_code>/', views.reserve_factor, name='reserve_factor'),
 ]
