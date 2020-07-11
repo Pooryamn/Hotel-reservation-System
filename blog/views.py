dcb91d3bdb61539c470e62654c6d3efcc86d292b
mahdigh8 <mahdighazaei8@gmail.com>
2020-07-11 13:01:45 +0430

    add paginator to reservation history page

--- blog/views.py
+++ blog/views.py
@@ -238,5 +238,18 @@ def reservation_history(request):
     user = request.user
     reserve_list = Reserve.objects.filter(user=user)
 
+    ### pagination reserve_list
+    paginator = Paginator(reserve_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+
+    try:
+        reserve_list = paginator.page(page)
+    except PageNotAnInteger:
+    # If page is not an integer deliver the first page
+        reserve_list = paginator.page(1)
+    except EmptyPage:
+    # If page is out of range deliver last page of results
+        reserve_list = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/hotel/reservation_history.html',
                  {'reserve_list': reserve_list, 'user':user})
