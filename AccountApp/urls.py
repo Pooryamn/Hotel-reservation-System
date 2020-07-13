2f414be000b2f37c68db541873b365eedd78ec4a
mahdigh8 <mahdighazaei8@gmail.com>
2020-07-13 12:41:29 +0430

    add logout view

--- AccountApp/urls.py
+++ AccountApp/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 from . import views
 
 urlpatterns = [
     # post views
-    url('login/',views.user_login,name='login'),
-    url('register/',views.register,name='register'),
-    url('confrim/',views.Confrim_check,name='Confrim_check'),
-    url('profile/',views.profile,name='profile'),
+    url(r'^login/$',views.user_login,name='login'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^confrim/$',views.Confrim_check,name='Confrim_check'),
+    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^logout/$', logout, name='logout'),
 ]
--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -21,6 +21,7 @@ EMAIL_PORT = EMAIL_PORT
 
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -165,3 +166,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# Logout Redirect
+LOGOUT_REDIRECT_URL = reverse_lazy('blog:home')
